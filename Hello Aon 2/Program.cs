5678932240bc055664f6f2a4c1c58c7997735909
hayder <hayderwheed15@gmail.com>
2024-10-08 19:06:57 +0300

    adding my task

---
+++ Hello Aon 2/Program.cs
@@ -0,0 +1,66 @@
+﻿
+
+Console.WriteLine("------------------");
+var enduser = "Hayder Wheed";
+var endpass = "123";
+
+Console.WriteLine("UserName:");
+var name = Console.ReadLine();
+Console.WriteLine("Password:");
+var pass = Console.ReadLine();
+
+if (name == enduser )
+{
+    Console.WriteLine("Username IS CORECT");
+}
+else
+{
+    Console.WriteLine("wrong! in username ");
+    Console.WriteLine("Credentials rejected ,exiting");
+}
+
+if (pass==endpass)
+{
+    Console.WriteLine("Password IS CORECT");   
+}
+
+else
+{
+    Console.WriteLine("Wrong in password");
+    Console.WriteLine("Credentials rejected ,exiting");
+    return;
+}
+if (string.IsNullOrEmpty(name))
+{
+    Console.WriteLine("user name canot be null or empty");
+    return;
+}
+if(string.IsNullOrEmpty(pass))
+{
+    Console.WriteLine("Passord can not be null or empty");
+    return;
+}
+
+Random random = new Random();
+int otp = random.Next(100000, 999999);
+
+if (pass == endpass  && name==enduser)
+{
+    Console.WriteLine($"your OTP is {otp}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
